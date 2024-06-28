9ee867f401e2109eef085b944ef5533d8586b22d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-06-28 11:27:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd 
+from st_aggrid import AgGrid
 
 house = pd.read_csv('house_clean.csv')
 
@@ -15,5 +16,7 @@ def main() :
 
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+  AgGrid(house)
+  
 if __name__ == '__main__' : 
   main()
