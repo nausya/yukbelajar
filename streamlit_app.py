2d929ff4ecf3fe9701292c6fed2236d24abbc1ac
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-06-28 11:33:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def main() :
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
+  #AgGrid(house)
   
   st.table([x for x in range(1,5)])
   
