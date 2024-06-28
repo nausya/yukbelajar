9e04498fd4c0e6566c897ebe0e30ec3743a4608c
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-06-28 11:16:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ def main() :
   st.dataframe(house)
 
   st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
 if __name__ == '__main__' : 
   main()
