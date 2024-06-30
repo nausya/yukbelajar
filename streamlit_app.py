228969fbf98138ec9390e76d019331fec2c5c599
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-06-30 23:06:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd 
 import pickle
-import streamlit as st
 
 model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
@@ -22,26 +21,8 @@ if st.button('Estimasi Harga'):
     st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
     st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*16500)
   
-##Membuat sidebar
-st.sidebar.title("Sidebar")
-input_text = st.sidebar.text_input("Masukkan sesuatu:")
-input_number = st.sidebar.number_input("Masukkan angka:", min_value=0, max_value=100)
 
-###Tombol untuk memindahkan konten
-if st.sidebar.button("Tampilkan di Mainbar"):
-    st.session_state['show_content'] = True
-else:
-    st.session_state['show_content'] = False
-
-#Menampilkan hasil di mainbar
-st.title("Mainbar")
-if 'show_content' in st.session_state and st.session_state['show_content']:
-    st.write(f"Teks dari sidebar: {input_text}")
-    st.write(f"Angka dari sidebar: {input_number}")
-else:
-    st.write("Tidak ada konten untuk ditampilkan.")
-
-def main() : 
+#def main() : 
   
-if __name__ == '__main__' : 
-  main()
+#if __name__ == '__main__' : 
+ # main()
