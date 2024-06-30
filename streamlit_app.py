ff584007c1d49e5b41b82f3b9039dcb087f5b431
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-06-30 23:13:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,21 @@ model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
 st.title('Estimasi Harga Mobil Bekas')
 
-year = st.number_input('Input Tahun Mobil')
-mileage = st.number_input('Input Km Mobil')
-tax = st.number_input('Input Pajak Mobil')
-mpg = st.number_input('Input Konsumsi BBM Mobil')
-engineSize = st.number_input('Input Engine Size')
+year = st.sidebar.number_input('Input Tahun Mobil')
+mileage = st.sidebar.number_input('Input Km Mobil')
+tax = st.sidebar.number_input('Input Pajak Mobil')
+mpg = st.sidebar.number_input('Input Konsumsi BBM Mobil')
+engineSize = st.sidebar.number_input('Input Engine Size')
 
-predict = ''
-
-if st.button('Estimasi Harga'):
-    predict = model.predict(
-        [[year, mileage, tax, mpg, engineSize]]
-    )
-    st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
-    st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*16500)
+def main() : 
+    predict = ''
+    
+    if st.sidebar.button('Estimasi Harga'):
+        predict = model.predict(
+            [[year, mileage, tax, mpg, engineSize]]
+        )
+        st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
+        st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*16500)
   
-
-#def main() : 
-  
-#if __name__ == '__main__' : 
- # main()
+if __name__ == '__main__' : 
+  main()
